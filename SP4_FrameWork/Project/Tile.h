5b39c6aa23a55a73bb1265933a542d4bfc9324a3
LoOsS_5@hotmail.com <LoOsS_5@hotmail.com@a4a325fe-63d8-b7d3-62f6-e8d87589361c>
2015-02-16 06:32:57 +0000

    add Tile class for map

---
+++ SP4_FrameWork/Project/Tile.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "vector3D.h"
+#include "TextureImage.h"
+
+class Tile
+{
+	enum
+	{
+		GRASS = 0,
+		SOIL,
+		ROCK,
+		BAD,
+		LAST
+	}TileType;
+	private:
+		int Type;
+		bool Empty;
+		Vector3D Position;
+		Vector3D Color;
+		float Alpha;
+		int Size;
+	public:
+		Tile(void);
+		~Tile(void);
+		TextureImage TileTexture;
+		void Init();
+		void Update();
+		void Draw(float x , float y);
+
+		int GetType();
+		bool GetEmpty();
+		Vector3D GetPosition();
+		Vector3D GetColor();
+
+		void SetType(int type);
+		void SetEmpty(bool e);
+		void SetPosition(Vector3D pos);
+		void SetColor(Vector3D c);
+
+};
+
