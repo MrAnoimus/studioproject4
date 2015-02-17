206db1ff230946fa5ddc25ad977069c87bd93e21
JustEsson@gmail.com <JustEsson@gmail.com@a4a325fe-63d8-b7d3-62f6-e8d87589361c>
2015-02-17 08:21:14 +0000

    Added visuals for Citizen and Movement for Citizen

--- trunk/Project/Citizen.cpp
+++ trunk/Project/Citizen.cpp
@@ -1,6 +1,7 @@
 #include "Citizen.h"
 
 
+
 Citizen::Citizen(void)
 	:TheMoods(Citizen::SAD)
 	,TheFavourites(Citizen::SLACK)
@@ -8,22 +9,78 @@ Citizen::Citizen(void)
 	,happy(false)
 	,HappinessLevels(0)
 	,active(true)
+	,AnimationCounter(0.0f)
+	,AnimationInvert(false)
 {
 	glEnable(GL_TEXTURE_2D);
-	LoadTGA(&Moody,"Textures/Smiley.tga");
-}
+	LoadTGA(&Okay, "Textures/Smiley.tga");
+	LoadTGA(&Happy, "Textures/Happy.tga");
+	LoadTGA(&Enraged, "Textures/Enraged.tga");
+	LoadTGA(&Sad, "Textures/Sad.tga");
+	LoadTGA(&FemaleCitizen, "Textures/FemaleCitizen.tga");
+	LoadTGA(&MaleCitizen, "Textures/MaleCitizen.tga");
+	srand(time(NULL));
 
+	int Random = rand() % 3 + 1;	
+	if(Random==1)
+	{
+		SetFavourite(Citizen::FOOD);
+	}
+	else if(Random==2)
+	{
+		SetFavourite(Citizen::SLACK);
+	}
+	else if(Random==3)
+	{
+		SetFavourite(Citizen::WORK);
+	}
+	srand(time(NULL));
+
+	int Random2 = rand() % 2 + 1;	
+	if(Random2==1)
+	{
+		Gender=1;
+	}
+	else
+	{
+		Gender=2;
+	}
+}
 
 Citizen::~Citizen(void)
 {
 }
 
-void Citizen::MoodUpdate(NearByBuilding TheBuilding, Favourite TheFavourite)
+void Citizen::MoodUpdate()
 {
-	switch(TheFavourite)
+	int time = glutGet(GLUT_ELAPSED_TIME);
+	static int ctime = glutGet(GLUT_ELAPSED_TIME);
+	
+	if(dir==1)
+	{
+		AnimationInvert=false;
+		SetPosition(Vector3D(GetPosition().x+1,GetPosition().y,GetPosition().z));
+	}
+	else if(dir==2)
+	{
+		AnimationInvert=true;
+		SetPosition(Vector3D(GetPosition().x-1,GetPosition().y,GetPosition().z));
+	}
+	if (time - ctime > 300) 
+	{
+		dir = rand() % 2 + 1;
+		this->AnimationCounter--;
+		if (this->AnimationCounter == 0)
+		{
+			this->AnimationCounter = 2;
+		}
+		ctime = time;
+	}
+	
+	switch(TheFavourites)
 	{
 	case Citizen::FOOD:
-		if(TheBuilding== Citizen::EATINGPLACE)
+		if(TheBuildings== Citizen::EATINGPLACE)
 		{
 			this->happy=true;
 		}
@@ -33,7 +90,7 @@ void Citizen::MoodUpdate(NearByBuilding TheBuilding, Favourite TheFavourite)
 		}
 		break;
 	case Citizen::WORK:
-		if(TheBuilding==Citizen::WORKPLACE)
+		if(TheBuildings==Citizen::WORKPLACE)
 		{
 			this->happy=true;
 		}
@@ -43,7 +100,7 @@ void Citizen::MoodUpdate(NearByBuilding TheBuilding, Favourite TheFavourite)
 		}
 		break;
 	case Citizen::SLACK:
-		if(TheBuilding==Citizen::NOTHING)
+		if(TheBuildings==Citizen::NOTHING)
 		{
 			this->happy=true;
 		}
@@ -90,8 +147,30 @@ void Citizen::MoodUpdate(NearByBuilding TheBuilding, Favourite TheFavourite)
 		this->TheMoods=Citizen::HAPPY;
 
 	}
-	std::cout <<"Happiness Level: " << HappinessLevels << std::endl;
+	/*std::cout <<"Happiness Level: " << HappinessLevels << std::endl;
 	std::cout <<"TheMood: " << GetMood() << std::endl;
+	std::cout <<"TheFavourite: " << GetFavourites() << std::endl;
+	std::cout <<"ThePlace: " << GetPlace() << std::endl;*/
+
+}
+
+std::string Citizen::GetFavourites(void)
+{
+	switch(TheFavourites)
+	{
+	case FOOD:
+		return "FOOD";
+		break;
+	case WORK:
+		return "WORK";
+		 break;
+	case SLACK:
+		return "SLACK";
+		break;
+	default:
+		return "DEFAULT";
+		break;
+	}
 }
 
 std::string Citizen::GetMood(void)
@@ -116,23 +195,143 @@ std::string Citizen::GetMood(void)
 	}
 }
 
+std::string Citizen::GetPlace(void)
+{
+	switch(TheBuildings)
+	{
+	case EATINGPLACE:
+		return "EATINGPLACE";
+		break;
+	case WORKPLACE:
+		return "WORKPLACE";
+		 break;
+	case NOTHING:
+		return "NOTHING";
+		break;
+	default:
+		return "DEFAULT";
+		break;
+	}
+}
+
 void Citizen::Draw()
 {
+	glEnable(GL_TEXTURE_2D);
 	glPushMatrix();
 	glTranslatef(0,0,-1);
 	
-	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D,Moody.id);
-	glBegin(GL_QUADS);
-				glTexCoord2f(0,0);
-				glVertex2f(-50,50);
-				glTexCoord2f(1,0);
-				glVertex2f(50,50);
-				glTexCoord2f(1,1);
-				glVertex2f(50,-50);
-				glTexCoord2f(0,1);
-				glVertex2f(-50,-50);
-		glEnd();
-		glDisable(GL_TEXTURE_2D);
+	if(this->GetMood()=="HAPPY")
+	{
+		glBindTexture(GL_TEXTURE_2D,Happy.id);
+	}
+	else if(this->GetMood()=="ENRAGED")
+	{
+		glBindTexture(GL_TEXTURE_2D, Enraged.id);
+	}
+	else if(this->GetMood()=="SAD")
+	{
+		glBindTexture(GL_TEXTURE_2D, Sad.id);
+	}
+	else if(this->GetMood()=="OKAY")
+	{
+		glBindTexture(GL_TEXTURE_2D, Okay.id);
+	}
+	DrawSquare(50,50);
+
+	
 	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(GetPosition().x,GetPosition().y,-2);
+	RenderCitizen();
+	glPopMatrix();
+
+	glDisable(GL_TEXTURE_2D);
+}
+
+void Citizen::SetFavourite(Favourite MyFavourite)
+{
+	this->TheFavourites = MyFavourite;
+}
+
+void Citizen::SetPlace(NearByBuilding myBuilding)
+{
+	this->TheBuildings = myBuilding;
+}
+
+void Citizen::RenderCitizen(void)
+{
+	glPushMatrix();
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_BLEND);
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	if(Gender==1)
+	{
+		glBindTexture(GL_TEXTURE_2D, FemaleCitizen.id);
+	}
+	else
+	{
+		glBindTexture(GL_TEXTURE_2D, MaleCitizen.id);
+	}
+	glBegin(GL_QUADS);
+	if(AnimationInvert==false)
+	{
+		glTexCoord2f(0.25 * AnimationCounter, 1); 
+		glVertex2f(50, 70);
+		glTexCoord2f(0.25 * AnimationCounter, 0); 
+		glVertex2f(50, 0);
+		glTexCoord2f(0.25 * AnimationCounter + 0.25, 0);
+		glVertex2f(0, 0);
+		glTexCoord2f(0.25 * AnimationCounter + 0.25, 1);
+		glVertex2f(0, 70);
+	}
+	else if(AnimationInvert==true)
+	{
+		glTexCoord2f(0.25 * AnimationCounter + 0.25, 1);
+		glVertex2f(50, 70);
+		glTexCoord2f(0.25 * AnimationCounter + 0.25, 0);
+		glVertex2f(70, 0);
+		glTexCoord2f(0.25 * AnimationCounter, 0);
+		glVertex2f(0,0);
+		glTexCoord2f(0.25 * AnimationCounter, 1); 
+		glVertex2f(0, 70);
+
+	}
+	glEnd();
+	glPopMatrix();
+}
+
+void Citizen::DrawSquare(float xSize, float ySize)
+{
+	glBegin(GL_QUADS);
+		glTexCoord2f(1,0);
+			glVertex2f(xSize,-ySize);
+		glTexCoord2f(1,1);
+			glVertex2f(xSize,ySize);
+		glTexCoord2f(0,1);
+			glVertex2f(-xSize,ySize);
+		glTexCoord2f(0,0);
+			glVertex2f(-xSize,-ySize);
+	glEnd();
+}
+
+void Citizen::SetAnimationCounter(int AnimationCounter)
+{
+	this->AnimationCounter=AnimationCounter;
+}
+
+int Citizen::GetAnimationCounter(void)
+{
+	return this->AnimationCounter;
+}
+
+void Citizen::SetPosition(Vector3D Position)
+{
+	this->Position = Position;
+}
+
+Vector3D Citizen::GetPosition(void)
+{
+	return this->Position;
 }
