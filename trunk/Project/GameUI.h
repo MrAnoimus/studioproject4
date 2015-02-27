b5a6152c7c6bf2a7d8ed3f06393acde07dc4855c
LoOsS_5@hotmail.com <LoOsS_5@hotmail.com@a4a325fe-63d8-b7d3-62f6-e8d87589361c>
2015-02-27 09:39:39 +0000

    [NEW] add texture update new ui stuff

--- trunk/Project/GameUI.h
+++ trunk/Project/GameUI.h
@@ -11,11 +11,16 @@ class GameUI
 		int size;
 	public:
 		TextureImage SelectionTexture[10];
+		TextureImage MoneyTexture;
+		TextureImage ManTexture;
+		TextureImage citizenTexture;
 		CGameTime myGameTime;
 		GameUI(void);
 		~GameUI(void);
 		void Init();
 		void Update();
+		void DrawResourceData(int x , int y,int money , int manpower , int citizennum);
+		void DrawResource(int x , int y);
 		void DrawSelect(int x , int y ,bool mode, int type);
 		void Draw(float x , float y);
 };
